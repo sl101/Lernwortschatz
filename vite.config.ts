d14438554bc681bbb54068c69a2b7cffc623360e
Slava <v.zhevaga@gmail.com>
2025-02-13 21:56:07 +0100

    body center fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+	base: '/Lernwortschatz/',
+  //base: './',
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'src'), 
