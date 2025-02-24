38efa2e8cbc0cbe51919d44a2ca69abc16f88050
Slava <v.zhevaga@gmail.com>
2025-02-24 16:46:37 +0100

    github ex

---
+++ src/assets/wiederholung/wortschatz_de.ts
@@ -0,0 +1,13 @@
+export default  [
+	{
+  "title": "die Vereinbarung, -en",
+  "subtitle": "(соглашение, договорённость)",
+  "erklärung": "Eine Vereinbarung ist eine gegenseitige Abmachung oder ein Vertrag zwischen zwei oder mehreren Parteien.",
+  "beispiele": [
+    "Wir haben eine mündliche Vereinbarung getroffen.",
+    "Die Vereinbarung wurde schriftlich festgehalten."
+  ],
+  "synonyme": "Abmachung, Vertrag, Abkommen",
+  "antonyme": "Streit, Konflikt, Uneinigkeit"
+}
+]
